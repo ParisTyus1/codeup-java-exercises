4647351e3b06667545e2a93b2f31e7131607928e
Paris Tyus <Tyus.Paris1@gmail.com>
2020-01-28 11:28:17 -0600

    initial push

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int myFavoriteNumber = 19;
+            System.out.println(myFavoriteNumber);
+        String myString = "My name is Paris";
+            System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;//tricking the compiler.//
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+    }
+
+}
+
