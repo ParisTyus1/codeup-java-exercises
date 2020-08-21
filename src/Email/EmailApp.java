c5d1cc759e3d9ddd7a032a0b0fb995e2cc02d90d
Paris Tyus <Tyus.Paris1@gmail.com>
2020-08-21 14:06:33 -0500

    updated git ignore

--- src/Email/EmailApp.java
+++ src/Email/EmailApp.java
@@ -5,5 +5,7 @@ public class EmailApp {
     public static void main(String[] args) {
     Email em1 = new Email("John", "Smith");
         System.out.println(em1.showInfo());
+    Email em2 = new Email("Briana", "Smith");
+        System.out.println(em2.showInfo());
     }
 }
